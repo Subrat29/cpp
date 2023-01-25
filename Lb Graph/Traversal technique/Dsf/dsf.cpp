8a40256dd56a99bc2384bef7ea7193d22dec0502
Subrat <ankubanku29062003@gmail.com>
2023-01-25 07:16:10 +0530

    24-1-23

---
+++ Lb Graph/Traversal technique/Dsf/dsf.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>  // linear tc, sc
+using namespace std; // ye sasur adj list ki kya jarurat h seedhe graph ko traverse ku ni krte, visited map banakr.
+
+void dfs(int node, unordered_map<int, bool> &visited, unordered_map<int, list<int>> &adj, vector<int> &component)
+{
+    //ans store
+    component.push_back(node);
+
+    //mark visited
+    visited[node] = true;
+
+    // recursive call for every connected node
+    for (auto i : adj[node])
+    {
+        if (!visited[i])
+        {
+            dfs(i, visited, adj, component);
+        }
+    }
+}
+
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+{
+    // prepare adlist
+    unordered_map<int, list<int>> adj;
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<vector<int>> ans;
+    unordered_map<int, bool> visited;
+
+    // for all nodes call dfs, if not visited
+    for (int i = 0; i < V; i++)
+    {
+        if (!visited[i])
+        {
+            vector<int> component;
+            dfs(i, visited, adj, component);
+            ans.push_back(component);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+
+    return 0;
+}
