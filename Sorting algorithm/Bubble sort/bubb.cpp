338376cd682270097a8f4fbb6d868af3dfb34ff6
Subrat29 <ankubanku29062003@gmail.com>
2023-08-21 21:57:48 +0530

    21-8

--- Sorting algorithm/Bubble sort/bubb.cpp
+++ Sorting algorithm/Bubble sort/bubb.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
 
-//last part remove in every round
+// last part remove in every round
 
-void bubbleSort(int arr[], int n) 
+void bubbleSort(int arr[], int n)
 {
     for (int i = 1; i < n; i++) // n-1 rounds // use only for no. of rounds
     {
         for (int j = 0; j < n - i; j++)
         {
             if (arr[j] > arr[j + 1])
+            {
                 swap(arr[j], arr[j + 1]);
+            }
         }
     }
 }
@@ -30,3 +32,30 @@ int main()
 
     return 0;
 }
+
+
+
+//For Dry Run Only
+
+// void bubbleSort(int arr[], int n)
+// {
+//     for (int i = 1; i < n; i++) // n-1 rounds // use only for no. of rounds
+//     {
+//         for (int j = 0; j < n - i; j++)
+//         {
+//             cout << "i : " << i << ",  j : " << j;
+//             cout << "  arr[j] = " << arr[j] << "  arr[j+1] = " << arr[j + 1] << endl;
+
+//             if (arr[j] > arr[j + 1])
+//             {
+//                 swap(arr[j], arr[j + 1]);
+//                 cout << "---> swap" << endl;
+//             }
+//             for (int i = 0; i < 6; i++)
+//             {
+//                 cout << arr[i] << " ";
+//             }cout << endl << endl;
+//         }
+//         cout << endl;
+//     }
+// }
